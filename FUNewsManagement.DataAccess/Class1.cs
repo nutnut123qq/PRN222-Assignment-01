7bd423271e6b2554ebc8abfd197b0f23a735091a
hoang <=>
2025-06-03 10:47:35 +0700

    Clean up: Remove redundant Class1.cs template files

--- FUNewsManagement.DataAccess/Class1.cs
+++
@@ -1,2 +0,0 @@
-﻿// This file can be removed as models are now in separate files
-// SystemAccount model has been moved to Models/SystemAccount.cs
--- FUNewsManagement.Services/Class1.cs
+++
@@ -1,2 +0,0 @@
-﻿// This file can be removed as interfaces are now in separate files
-// ISystemAccountService has been moved to ISystemAccountService.cs
